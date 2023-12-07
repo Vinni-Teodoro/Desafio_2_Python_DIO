3a7903375ed44cec2c7bb365749bed976a4ae0e1
Vinni-Teodoro <vinicius_adriano_teodoro@hotmail.com>
2023-12-07 20:54:26 -0300

    1

---
+++ Desafio_2_Python_DIO.py
@@ -0,0 +1,21 @@
+month = input("Digite o número de 1 à 12:")
+
+months_dict = {
+    "1" : "January",
+    "2" : "February",
+    "3" : "March",
+    "4" : "April",
+    "5" : "May",
+    "6" : "June",
+    "7" : "July",
+    "8" : "August",
+    "9" : "September",
+    "10" : "October",
+    "11" : "November",
+    "12" : "December"
+
+}
+
+months= months_dict[f"{month}"]
+
+print(months)
